886a1d325ef935fe9cdafa1f1df6cef4b212152b
Gdevp <128643492+Gdevp@users.noreply.github.com>
2024-01-30 16:12:25 -0500

    Update blocks.h

--- blocks.h
+++ blocks.h
@@ -15,8 +15,25 @@ static const Block blocks[] = {
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
 static char delim[] = " | ";
 static unsigned int delimLen = 5;
+// -----------------------------------------------------------------------------------------------------------------/
+/*EDIT SECON SETTINS*/
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+// Declaración o definición de status
 
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"   ", "nmcli -t -f NAME connection show --active", 5, 0},
 
+	{"Mem: ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+
+	{"battery ", "/usr/local/bin/battery-notification.sh", 60, 0},
+
+	{"", "date '+%b %d (%a) %I:%M%p'",        5,		0},
+};
+
+//sets delimiter between status commands. NULL character ('\0') means no delimiter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
 
 
 
